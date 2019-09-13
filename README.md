bab9d143f0de3b62a94bc4d7e2ea4ba0205d01e6
Cristian Gonzalez Rojas <cristian.gonzalez.rojas@everis.com>
2019-09-13 12:00:41 -0300

    first commit archetype for springboot with mongodb

---
+++ README.md
@@ -0,0 +1,18 @@
+restful-springboot-data-mongodb
+=========
+
+Fuentes del arquetipo RestFul SpringBoot backend para envio de correo.
+Para compilar
+```
+mvn clean install
+```
+Para ejecutar
+```
+mvn spring-boot:run
+```
+
+##### USO
+Documentacion y uso de Swagger
+```
+http://localhost:8080/swagger-ui.html#/
+```
---
+++ src/main/java/cl/dataservices/domainmongo/DomainMongoApplication.java
@@ -0,0 +1,13 @@
+package cl.dataservices.domainmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainMongoApplication.class, args);
+	}
+
+}
